e8f70d36e583bc75bf79b43c957c1e685ff3c14c
JMcB <joel.mcbride1@live.com>
2021-03-10 16:16:53 +0000

    Add keep_alive.py for repl.it hosting

---
+++ keep_alive.py
@@ -0,0 +1,19 @@
+# copied from https://github.com/ClarityCoders/Reddit-Bot-Replit/blob/master/keep_alive.py
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+
+@app.route('/')
+def home():
+    return "Server Alive!"
+
+
+def run():
+    app.run(host='0.0.0.0', port=8080)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
